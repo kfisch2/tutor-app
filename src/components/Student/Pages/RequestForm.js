79136112fe9601ced753be69261d81264d756243
kfisch2 <kmlindsey24@gmail.com>
2023-02-08 14:00:49 -0800

    useLazyQuery fixed with onComplete cb functiongit add .!

--- src/components/Student/Pages/RequestForm.js
+++ src/components/Student/Pages/RequestForm.js
@@ -5,39 +5,58 @@ import { QUERY_TUTORS_BY_SUBJECT } from "../../../utils/queries";
 
 const RequestForm = () => {
   const { data: subjectData } = useQuery(QUERY_SUBJECTS);
-  const [
-    getTutors,
-    { loading: loadingTutor, error: tutorError, data: tutorData },
-  ] = useLazyQuery(QUERY_TUTORS_BY_SUBJECT, {
-    fetchPolicy: "network-only",
-  });
+  const [getTutors, { called, loading, data }] = useLazyQuery(
+    QUERY_TUTORS_BY_SUBJECT,
+    {
+      fetchPolicy: "network-only",
+      // onCompleted: (response) => {
+      //   for (let i = 0; i < response.length; i++) {
+      //     return <li>{response[i]}</li>;
+      //   }
+      // },
+    }
+  );
   // POPULATE SUBJECTS
 
-  const populateTutors = () => {
-    loadingTutor
-      ? console.log("loading tutors...")
-      : console.log("not loading tutors");
-    tutorError
-      ? console.log("error loading tutors")
-      : console.log("no error loading tutuors");
-    tutorData ? console.log(tutorData) : console.log("no tutor data");
+  const handleSubmit = (subject) => {
+    getTutors({
+      variables: { subjects: subject },
+      onCompleted: (response) => {},
+    });
   };
+
+  const renderTutors = (response) => {
+    const tutor = response.tutorBySubject;
+    for (let i = 0; i < tutor.length; i++) {
+      console.log(tutor[i]);
+    }
+  };
+
   return (
     <>
-      <h1 onClick={() => populateTutors()}>Request Form!</h1>
+      <h1>Request Form!</h1>
       <h2>Science: </h2>
       {subjectData?.subjects[0].science.map((subject) => {
         return (
-          <button
-            onClick={() => {
-              getTutors({ variables: { subjects: subject } });
-              populateTutors();
-            }}
-            className={subject}
-            key={subject}
-          >
-            {subject}
-          </button>
+          <>
+            <button
+              onClick={() => {
+                getTutors({
+                  variables: { subjects: subject },
+                  onCompleted: (response) => {
+                    renderTutors(response);
+                  },
+                });
+              }}
+              className={subject}
+              key={subject}
+            >
+              {subject}
+            </button>
+            {/* <ul>
+              {data?.tutorBySubject && <li>{[data.tutorBySubject[0]]}</li>}
+            </ul> */}
+          </>
         );
       })}
 
@@ -46,8 +65,8 @@ const RequestForm = () => {
         return (
           <button
             onClick={() => {
-              getTutors({ variables: { subjects: subject } });
-              populateTutors();
+              // getTutors({ variables: { subjects: subject } });
+              // populateTutors();
             }}
             className={subject}
             key={subject}
@@ -62,8 +81,8 @@ const RequestForm = () => {
         return (
           <button
             onClick={() => {
-              getTutors({ variables: { subjects: subject } });
-              populateTutors();
+              // getTutors({ variables: { subjects: subject } });
+              // populateTutors();
             }}
             className={subject}
             key={subject}
@@ -78,8 +97,8 @@ const RequestForm = () => {
         return (
           <button
             onClick={() => {
-              getTutors({ variables: { subjects: subject } });
-              populateTutors();
+              // getTutors({ variables: { subjects: subject } });
+              // populateTutors();
             }}
             className={subject}
             key={subject}
@@ -94,8 +113,8 @@ const RequestForm = () => {
         return (
           <button
             onClick={() => {
-              getTutors({ variables: { subjects: subject } });
-              populateTutors();
+              // getTutors({ variables: { subjects: subject } });
+              // populateTutors();
             }}
             className={subject}
             key={subject}
