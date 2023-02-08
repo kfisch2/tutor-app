a8b7a8323a160c56b991ed13bde8fea0da3903c2
kfisch2 <kmlindsey24@gmail.com>
2023-02-08 15:15:20 -0800

    tutors render in a list next to the form

--- src/components/Student/Pages/RequestForm.js
+++ src/components/Student/Pages/RequestForm.js
@@ -4,44 +4,56 @@ import { QUERY_SUBJECTS } from "../../../utils/queries";
 import { QUERY_TUTORS_BY_SUBJECT } from "../../../utils/queries";
 
 const RequestForm = () => {
-  const { data: subjectData } = useQuery(QUERY_SUBJECTS);
-  const [getTutors, { called, loading, data }] = useLazyQuery(
-    QUERY_TUTORS_BY_SUBJECT,
-    {
-      fetchPolicy: "network-only",
-      // onCompleted: (response) => {
-      //   for (let i = 0; i < response.length; i++) {
-      //     return <li>{response[i]}</li>;
-      //   }
-      // },
-    }
-  );
   // POPULATE SUBJECTS
+  const { data: subjectData } = useQuery(QUERY_SUBJECTS);
 
-  const handleSubmit = (subject) => {
-    getTutors({
-      variables: { subjects: subject },
-      onCompleted: (response) => {},
-    });
-  };
+  const [fetchTutors] = useLazyQuery(QUERY_TUTORS_BY_SUBJECT);
 
   const renderTutors = (response) => {
     const tutor = response.tutorBySubject;
-    for (let i = 0; i < tutor.length; i++) {
-      console.log(tutor[i]);
+    const tutorList = document.querySelector(".tutorList");
+    console.log(`${tutor.length} tutor(s) available`);
+    if (tutor) {
+      for (let i = 0; i < tutor.length; i++) {
+        const individualTutor = document.createElement("li");
+        individualTutor.textContent = tutor[i].username;
+        tutorList.appendChild(individualTutor);
+      }
     }
   };
 
   return (
-    <>
-      <h1>Request Form!</h1>
-      <h2>Science: </h2>
-      {subjectData?.subjects[0].science.map((subject) => {
-        return (
-          <>
+    <div className="requestFormPage">
+      <div className="subjectList">
+        <h1>Request Form!</h1>
+        <h2>Science: </h2>
+        {subjectData?.subjects[0].science.map((subject) => {
+          return (
+            <>
+              <button
+                onClick={() => {
+                  fetchTutors({
+                    variables: { subjects: subject },
+                    onCompleted: (response) => {
+                      renderTutors(response);
+                    },
+                  });
+                }}
+                className={subject}
+                key={subject}
+              >
+                {subject}
+              </button>
+            </>
+          );
+        })}
+
+        <h2>Mathematics: </h2>
+        {subjectData?.subjects[0].mathematics.map((subject, i) => {
+          return (
             <button
               onClick={() => {
-                getTutors({
+                fetchTutors({
                   variables: { subjects: subject },
                   onCompleted: (response) => {
                     renderTutors(response);
@@ -53,77 +65,74 @@ const RequestForm = () => {
             >
               {subject}
             </button>
-            {/* <ul>
-              {data?.tutorBySubject && <li>{[data.tutorBySubject[0]]}</li>}
-            </ul> */}
-          </>
-        );
-      })}
+          );
+        })}
 
-      <h2>Mathematics: </h2>
-      {subjectData?.subjects[0].mathematics.map((subject, i) => {
-        return (
-          <button
-            onClick={() => {
-              // getTutors({ variables: { subjects: subject } });
-              // populateTutors();
-            }}
-            className={subject}
-            key={subject}
-          >
-            {subject}
-          </button>
-        );
-      })}
+        <h2>History: </h2>
+        {subjectData?.subjects[0].history.map((subject, i) => {
+          return (
+            <button
+              onClick={() => {
+                fetchTutors({
+                  variables: { subjects: subject },
+                  onCompleted: (response) => {
+                    renderTutors(response);
+                  },
+                });
+              }}
+              className={subject}
+              key={subject}
+            >
+              {subject}
+            </button>
+          );
+        })}
 
-      <h2>History: </h2>
-      {subjectData?.subjects[0].history.map((subject, i) => {
-        return (
-          <button
-            onClick={() => {
-              // getTutors({ variables: { subjects: subject } });
-              // populateTutors();
-            }}
-            className={subject}
-            key={subject}
-          >
-            {subject}
-          </button>
-        );
-      })}
+        <h2>Language: </h2>
+        {subjectData?.subjects[0].language.map((subject, i) => {
+          return (
+            <button
+              onClick={() => {
+                fetchTutors({
+                  variables: { subjects: subject },
+                  onCompleted: (response) => {
+                    renderTutors(response);
+                  },
+                });
+              }}
+              className={subject}
+              key={subject}
+            >
+              {subject}
+            </button>
+          );
+        })}
 
-      <h2>Language: </h2>
-      {subjectData?.subjects[0].language.map((subject, i) => {
-        return (
-          <button
-            onClick={() => {
-              // getTutors({ variables: { subjects: subject } });
-              // populateTutors();
-            }}
-            className={subject}
-            key={subject}
-          >
-            {subject}
-          </button>
-        );
-      })}
-
-      <h2>Social Science: </h2>
-      {subjectData?.subjects[0].socialScience.map((subject, i) => {
-        return (
-          <button
-            onClick={() => {
-              // getTutors({ variables: { subjects: subject } });
-              // populateTutors();
-            }}
-            className={subject}
-            key={subject}
-          >
-            {subject}
-          </button>
-        );
-      })}
-    </>
+        <h2>Social Science: </h2>
+        {subjectData?.subjects[0].socialScience.map((subject, i) => {
+          return (
+            <button
+              onClick={() => {
+                fetchTutors({
+                  variables: { subjects: subject },
+                  onCompleted: (response) => {
+                    renderTutors(response);
+                  },
+                });
+              }}
+              className={subject}
+              key={subject}
+            >
+              {subject}
+            </button>
+          );
+        })}
+      </div>
+      <div className="tutorList">
+        <h1>Available Tutors:</h1>
+        <ul className="tutorList"/>
+      </div>
+    </div>
   );
 };
 
