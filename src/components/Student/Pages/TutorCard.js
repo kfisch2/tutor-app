d82fd815f9fc0b45d9ef9dfa7b48dd9d3c83754a
kfisch2 <kmlindsey24@gmail.com>
2023-02-23 18:21:31 -0800

    adding stuff before pulling

--- src/components/Student/Pages/TutorCard.js
+++ src/components/Student/Pages/TutorCard.js
@@ -1,43 +1,60 @@
 import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 const TutorCard = ({ queryCalled, data, requestedCost, requestedSubject }) => {
-  const tutorInfo = document.querySelector(".tutorInfo");
-
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   if (queryCalled) {
-    tutorInfo.textContent = "";
     if (data) {
       const filteredData = data.filter((tutor) => tutor.cost <= requestedCost);
       if (filteredData.length == 0) {
-        tutorInfo.textContent =
-          "Sorry, no tutors matching your request found :(";
+        return <>No tutors found</>;
       } else {
-        for (let i = 0; i < filteredData.length; i++) {
-          const tutorContainer = document.createElement("div");
-          const username = document.createElement("h3");
-          const cost = document.createElement("li");
-          // const credentials = document.createElement('li');
-          // const bio = document.createElement('p');
-          username.textContent = filteredData[i].username;
-          cost.textContent = `$${filteredData[i].cost}/hr`;
-          // credentials.textContent = `Credentials: ${filteredData[i].credentials}`;
-          // bio.textContent = filteredData[i].bio;
-          tutorContainer.appendChild(username);
-          tutorContainer.appendChild(cost);
-          // tutorContainer.appendChild(credentials);
-          // tutorContainer.appendChild(bio);
-          tutorInfo.appendChild(tutorContainer);
-          tutorContainer.setAttribute("class", "tutorCard col");
-        }
+        return (
+          <>
+            {/* TUTOR CARD */}
+            <ul>
+              {filteredData.map((tutor, i) => (
+                <>
+                  {" "}
+                  <Modal show={show} onHide={handleClose} centered>
+                    <Modal.Header>
+                      <Modal.Title>{tutor.username}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div>{tutor.bio}</div>
+                      <div>{tutor.credentials}</div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button
+                        variant="dark"
+                        onClick={() => console.log("profile")}
+                      >
+                        Go to {tutor.username}'s profile
+                      </Button>{" "}
+                      <Button variant="light" onClick={handleClose}>
+                        Close
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                  <li key={i} className="tutorCard">
+                    <div onClick={handleShow}>
+                      <h2>{tutor.username}</h2>
+                      <p>${tutor.cost}/hr</p>
+                    </div>{" "}
+                  </li>{" "}
+                </>
+              ))}{" "}
+            </ul>
+            {/* MODAL */}
+          </>
+        );
       }
     }
   }
-  return <div className="tutorInfo row">Find your next tutor soon!</div>;
 };
 
 export default TutorCard;
